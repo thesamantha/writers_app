05943446d355fedc00d907adec5440659fe262ea
Samantha Parul Webber <webber@campus.tu-berlin.de>
2015-10-24 22:27:29 -0400

    Add a full_title helper

--- test/controllers/login_controller_test.rb
+++ test/controllers/login_controller_test.rb
@@ -5,29 +5,37 @@ class LoginControllerTest < ActionController::TestCase
   #   assert true
   # end
 
+  def setup
+    @base_title = "Writer's App"
+  end
+
   test "should get welcome" do
     get :welcome
     assert_response :success
-    assert_select "title", "Welcome | Writer's App"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get register" do
     get :register
     assert_response :success
+    assert_select "title", "Register | #{@base_title}"
   end
 
   test "should get help" do
-    get :register
+    get :help
     assert_response :success
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | #{@base_title}"
   end
 end
